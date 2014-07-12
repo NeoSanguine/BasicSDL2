1eb5a771d19021bee5b5b1906e0914123070ed03
NeoSanguine <Crimsonxlegend@gmail.com>
2014-07-12 07:32:08 -0500

    GameEngie class

---
+++ A very basic 2d attempt at a game/GameEngine.cpp
@@ -0,0 +1,66 @@
+#include "GameEngine.h"
+
+using namespace std;
+
+bool GameEngine::Initialize()
+{
+
+	//Just a place holder
+	bool success = true;
+	if (!SDL_Init(SDL_INIT_EVERYTHING))
+	{
+		std::cout << "Failed to Initialize SDL \n" << SDL_GetError();
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow("Simply Platform", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN&&SDL_WINDOW_BORDERLESS);
+		if (!gWindow)
+			std::cout << "Failed to Create Window \n" << SDL_GetError();
+		else
+		{
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer = NULL)
+				cout << "Failed to Create Renderer \n" << SDL_GetError();
+			else
+			{
+				screen = SDL_GetWindowSurface(gWindow);
+				//Add code here
+			}
+		}
+	}
+	return success;
+}
+
+void GameEngine::HandleEvents()
+{
+	if (getEvent().type == SDL_QUIT)
+		Quit();
+	if (getEvent().key.keysym.sym == SDLK_F1)
+		Quit();
+}
+
+void GameEngine::Update()
+{
+
+}
+
+void GameEngine::Render()
+{
+	SDL_RenderClear(getRenderer());
+	//Add render code here
+	SDL_RenderPresent(getRenderer());
+	
+}
+
+void GameEngine::Clean()
+{
+	SDL_DestroyWindow(gWindow);
+	SDL_DestroyRenderer(gRenderer);
+	SDL_FreeSurface(screen);
+
+	gWindow = NULL;
+	gRenderer = NULL;
+	screen = NULL;
+
+	SDL_Quit();
+}
---
+++ A very basic 2d attempt at a game/GameEngine.h
@@ -0,0 +1,46 @@
+#ifndef GAMEENGINE_H_
+#define GAMEENGINE_H_
+
+#pragma once
+#include <SDL.h>
+#include <iostream>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+#include <vector>
+
+struct GameEngine
+{
+	
+	GameEngine(){}
+	//Standard engine setup
+	bool Initialize();
+	void HandleEvents();
+	void Update();
+	void Render();
+	void Clean();
+
+	//Helper function to check if gRunning is true of false
+	bool Running(){ return gRunning; }
+	//Returns the window surface
+	SDL_Surface*getScreen(){ return screen; }
+	//Gets the event handler
+	SDL_Event getEvent(){ return gEvent; }
+	//Calls for gRunning to be false. i.e : Ends the game loop
+	bool Quit(){ return gRunning = false; }
+	//Gets the current renderer
+	SDL_Renderer*getRenderer(){ return gRenderer; }
+
+
+private:
+	bool gRunning;
+	SDL_Window*gWindow = NULL;
+	SDL_Renderer*gRenderer = NULL;
+	SDL_Surface*screen = NULL;
+	SDL_Event gEvent;
+	
+};
+
+
+
+#endif //GAMEENGINE_H_
